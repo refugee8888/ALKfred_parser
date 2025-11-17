1e933642f483538b59521c65b2e91f999e3610ce
Paul Ostaci <paulostaci88@gmail.com>
2025-11-17 13:48:31 +0000

    postgresql connection test working

--- src/alkfred/postgres_test.py
+++ src/alkfred/postgres_test.py
@@ -1,8 +1,10 @@
 
+from dotenv import load_dotenv
 from alkfred import config
+import psycopg2 as pg
 
 
-
+load_dotenv()
 conn = config.postgres_conn()
 cur = conn.cursor()
 cur.execute("SELECT version();")
