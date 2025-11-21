cc204c16d7b093b4a91fbecda2c51732ec9d8544
refugee8888 <paulostaci88@gmail.com>
2025-11-21 04:50:39 +0000

    postgres schema created and uploaded; found a key type mismatch in my previous sqlite schema and fixed it;

--- src/alkfred/sql/stg_load/civic_stg_evidence.py
+++ src/alkfred/sql/stg_load/civic_stg_evidence.py
@@ -5,7 +5,7 @@ from alkfred import config
 import uuid
 
 
-DB_PATH = config.default_db_path()
+
 JSON_PATH = Path("/app/data/civic_raw_evidence_db.json")
 unique_key_generator = config.UniqueKeyGenerator(initial_keys_list=set(config.load_from_json("data/unique_keys_list.json")) or None)
 
@@ -14,10 +14,10 @@ def main():
 
     logger = logging.getLogger(__name__)
 
-    conn = config.get_conn(DB_PATH)
+    conn = config.postgres_conn()
     cur = conn.cursor()
 
-    logger.info("Table civic_stg_evidence created or already exists in %s", DB_PATH)
+    # logger.info("Table civic_stg_evidence created or already exists in %s")
 
     data_dict = config.raw_json_list_to_dict(JSON_PATH)
 
